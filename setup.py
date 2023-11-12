701d0eadb1cc794ac33660ec7da95513a009330a
Larry Cai <larry.caiyu@gmail.com>
2023-11-12 17:50:50 +0100

    change
    Signed-off-by: Larry Cai <larry.caiyu@gmail.com>

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setuptools.setup(
     name="ben",
     version="0.2.0",
     description="bridge engine - ai system",
-    packages=setuptools.find_packages(include=['ben/*']),
-    #package_dir={'ben':'src'},
+    packages=setuptools.find_packages(include=['ben']),
+#    package_dir={'ben':'ben'},
 #    install_requires=[
 #        'requests',
 #        'markdown'
